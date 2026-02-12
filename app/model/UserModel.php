96f4b8ad97227f582667a319e8f8728dc3b1a3e0
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-02-12 13:59:44 +0300

    add: model

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -11,30 +11,32 @@ class UserModel {
     }
 
     public function getUserById($id) {
-        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = :id");
+        $stmt = $this->db->prepare("SELECT * FROM user WHERE id = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function getUserByEmailAndMdp($email, $mdp) {
-        $stmt = $this->db->prepare("SELECT * FROM users WHERE email = :email AND mdp = :mdp");
+    public function getUserByEmail($email) {
+        $stmt = $this->db->prepare("SELECT * FROM user WHERE email = :email");
         $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':mdp', $mdp);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_ASSOC);
     }
 
-    public function createUser($username, $password, $role) {
-        $stmt = $this->db->prepare("INSERT INTO users (nom, mdp, id_role) VALUES (:username, :password, :role)");
+    public function createUser($username, $prenom, $email, $password, $telephone, $role) {
+        $stmt = $this->db->prepare("INSERT INTO user (nom, prenom, email, mdp, telephone, id_role) VALUES (:username, :prenom, :email, :password, :telephone,  :role)");
         $stmt->bindParam(':username', $username);
-        $stmt->bindParam(':password', password_hash($password, PASSWORD_BCRYPT));
+        $stmt->bindParam(':prenom', $prenom);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':telephone', $telephone);
         $stmt->bindParam(':role', $role, PDO::PARAM_INT);
         return $stmt->execute();
     }  
 
     public function updateUser($id, $username, $password, $role) {
-        $stmt = $this->db->prepare("UPDATE users SET nom = :username, mdp = :password, id_role = :role WHERE id = :id");
+        $stmt = $this->db->prepare("UPDATE user SET nom = :username, mdp = :password, id_role = :role WHERE id = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         $stmt->bindParam(':username', $username);
         $stmt->bindParam(':password', password_hash($password, PASSWORD_BCRYPT));
@@ -43,9 +45,16 @@ class UserModel {
     }
 
     public function deleteUser($id) {
-        $stmt = $this->db->prepare("DELETE FROM users WHERE id = :id");
+        $stmt = $this->db->prepare("DELETE FROM user WHERE id = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         return $stmt->execute();
+    }
+
+    public function emailExists($email) {
+        $stmt = $this->db->prepare("SELECT COUNT(*) FROM user WHERE email = :email");
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        return $stmt->fetchColumn() > 0;
     }
 
 }
