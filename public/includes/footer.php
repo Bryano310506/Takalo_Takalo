2fc6e185fe8ae5c0301d4cdc1d8ab6a844276b8d
Bryano310506 <yvanandrianiaina3@gmail.com>
2026-02-12 21:56:27 +0300

    add: separation footer et header

---
+++ public/includes/footer.php
@@ -0,0 +1,16 @@
+    <!-- Footer -->
+    <footer class="footer text-center">
+        <div class="container">
+            <p class="mb-3">&copy; 2026 Takalo Takalo. Tous droits réservés.</p>
+            <div>
+                <a href="#" class="me-3"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="me-3"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="me-3"><i class="bi bi-instagram"></i></a>
+                <a href="#"><i class="bi bi-linkedin"></i></a>
+            </div>
+        </div>
+    </footer>
+
+    <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
